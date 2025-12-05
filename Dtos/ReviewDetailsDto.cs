4f06976bbf6064a40d92820d04f4eb7317ae8b94
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 23:24:31 +0200

    add review dtos

---
+++ Dtos/ReviewDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RentApi.Dtos
+{
+    public class ReviewDetailsDto
+    {
+        public int Id { get; set; }
+        public double Rating { get; set; }
+        public string? Comment { get; set; }
+
+        public string UserName { get; set; }
+        public string PropertyTitle { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Dtos/ReviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentApi.Dtos
+{
+    public class ReviewDto
+    {
+        [Required]
+        public int PropertyId { get; set; }
+
+        [Range(1, 5)]
+        public double Rating { get; set; }
+
+        public string? Comment { get; set; }
+    }
+}
