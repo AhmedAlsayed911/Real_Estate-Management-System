406610588808f853b23c81d8916aa2e765b2b658
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 23:24:07 +0200

    add models

---
+++ Models/Review.cs
@@ -0,0 +1,16 @@
+﻿namespace RentApi.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int PropertyId { get; set; }
+        public int UserId { get; set; }
+
+        public double Rating { get; set; } 
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Property Property { get; set; }
+        public User User { get; set; }
+    }
+}
