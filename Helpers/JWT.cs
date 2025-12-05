12ea7bbaa6d59b2006fdf4fd47e4c6edf6894439
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 23:24:36 +0200

    add auth helpers

---
+++ Helpers/JWT.cs
@@ -0,0 +1,10 @@
+﻿namespace RentApi.Helpers
+{
+    public class JWT
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int LifeTime { get; set; }
+        public string SigningKey { get; set; }
+    }
+}
