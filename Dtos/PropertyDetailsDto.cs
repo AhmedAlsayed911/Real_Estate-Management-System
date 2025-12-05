e18d5aa85ede194ecc60efb54f082c7710ec6bca
Ahmed Elsayed <ahmedsayed42032@gmail.com>
2025-12-05 23:24:26 +0200

    add property dtos

---
+++ Dtos/PropertyDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RentApi.Dtos
+{
+    public class PropertyDetailsDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal PricePerNight { get; set; }
+        public string Location { get; set; }
+        public string OwnerName { get; set; }
+        public string MainImageUrl { get; set; }
+        public int BookingCount { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
---
+++ Dtos/PropertyDtoUpdate.cs
@@ -0,0 +1,12 @@
+﻿namespace RentApi.Dtos
+{
+    public class PropertyDtoUpdate
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public decimal? PricePerNight { get; set; }
+        public IFormFile? MainImageUrl { get; set; }
+
+    }
+}
